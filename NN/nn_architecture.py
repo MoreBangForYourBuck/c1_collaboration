7c359d26184dd7a2007063e29be3c06180fc4c34
Jacob Gerlach <jwgerlach00@gmail.com>
2023-03-19 17:03:57 -0400

    It runs but is shitty

--- NN/nn_architecture.py
+++ NN/nn_architecture.py
@@ -3,28 +3,32 @@ import torch
 
 
 class NNModel(nn.Module):
-    def __init__(self):
+    def __init__(self, num_classes):
         super(NNModel, self).__init__()
-        self.fc1 = nn.Linear(64, 64)
+        self.fc1 = nn.Linear(6, 64)
         self.fc2 = nn.Linear(64, 64)
-        self.fc3 = nn.Linear(64, 1)
+        self.fc3 = nn.Linear(64, num_classes)
         self.relu = nn.ReLU()
+        self.output = nn.Softmax(dim=1)
         
     def forward(self, x):
         x = self.fc1(x)
         x = self.relu(x)
         x = self.fc2(x)
         x = self.relu(x)
-        return self.fc3(x)
+        x = self.fc3(x)
+        return self.output(x)
 
 
 class NNDataset(torch.utils.data.Dataset):
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    
+    def __init__(self, X, y):
+        self.X = torch.tensor(X, device=NNDataset.device).float()
+        self.y = torch.tensor(y, device=NNDataset.device)
     
     def __len__(self):
-        return len(self.x)
+        return len(self.X)
     
     def __getitem__(self, idx):
-        return self.x[idx], self.y[idx]
+        return self.X[idx], self.y[idx]
--- NN/nn_training_loop.py
+++ NN/nn_training_loop.py
@@ -6,38 +6,43 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import yaml
+import torch.nn.functional as F
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 
 def training_loop(imu, ann, hyperparams:dict):
-    model = NNModel()
-    print(model.parameters())
+    model = NNModel(num_classes=hyperparams['num_classes'])
+    model.to(device)
     optimizer = torch.optim.Adam(model.parameters(), lr=hyperparams['learning_rate'])
-    # criterion = torch.nn.CrossEntropyLoss() # one-hot encoding taken care of by pytorch
+    criterion = torch.nn.CrossEntropyLoss() # one-hot encoding taken care of by pytorch
 
-    print(imu, ann)
-    train_generator = DataLoader(NNDataset(imu, ann), batch_size=hyperparams['batch_size']) # DONT SHUFFLE (TIME SERIES)
+    train_generator = DataLoader(NNDataset(imu, ann), batch_size=hyperparams['batch_size'], shuffle=True) # DONT SHUFFLE (TIME SERIES)
+
 
     train_loss_history = []
     for epoch in range(1, hyperparams['epochs'] + 1):
         
         batch_train_loss_history = []
-        for (X, y) in train_generator:
+        for i, (X, y) in enumerate(train_generator):
+            print(i)
             optimizer.zero_grad()
             model.train()
             
             y_p = model(X)
             loss = criterion(y_p, y)
-            batch_train_loss_history.append(loss.item())
-            
+            # print(y, y_p)
+
             loss.backward()
             optimizer.step()
+            batch_train_loss_history.append(loss.item())
         
         # Append average loss across batches
         train_loss_history.append(sum(batch_train_loss_history) / len(batch_train_loss_history))
         
     plt.figure()
     plt.title('MSE Loss')
-    plt.plot(range(hyperparams['epoch']), train_loss_history, label='train loss')
+    plt.plot(range(hyperparams['epochs']), train_loss_history, label='train loss')
     plt.xlabel('Epochs')
     plt.ylabel('Loss')
     plt.legend()
@@ -63,5 +68,5 @@ if __name__ == '__main__':
     with open('NN/nn_hyperparams.yaml', 'r') as f:
         hyperparams = yaml.safe_load(f)
         
-    training_loop(hyperparams, imu.to_numpy(), np.array(ann))
+    training_loop(imu.to_numpy(), np.array(ann), hyperparams)
     
