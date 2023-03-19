18abb95977a029546ad791b5265c49a03d92e969
Jacob Gerlach <jwgerlach00@gmail.com>
2023-03-19 19:07:11 -0400

    lowered learning rate, cleaned crap out of training_loop. Trying training again but with loss stored every batch rather than epoch

--- NN/nn_training_loop.py
+++ NN/nn_training_loop.py
@@ -1,9 +1,7 @@
 import torch
 from torch.utils.data import DataLoader
 from nn_architecture import NNDataset, NNModel
-from helpers.preprocessing import expand_ann, read_all_data
-import pandas as pd
-import numpy as np
+from helpers.preprocessing import read_all_data
 import matplotlib.pyplot as plt
 import yaml
 from tqdm import tqdm
@@ -17,8 +15,8 @@ def training_loop(imu, ann, hyperparams:dict):
     optimizer = torch.optim.Adam(model.parameters(), lr=hyperparams['learning_rate'])
     criterion = torch.nn.CrossEntropyLoss() # one-hot encoding taken care of by pytorch
 
-    train_generator = DataLoader(NNDataset(imu, ann), batch_size=hyperparams['batch_size'], shuffle=True) # DONT SHUFFLE (TIME SERIES)
-
+    # Time series, but still shuffling because no window component
+    train_generator = DataLoader(NNDataset(imu, ann), batch_size=hyperparams['batch_size'], shuffle=True)
 
     train_loss_history = []
     for epoch in range(1, hyperparams['epochs'] + 1):
@@ -42,7 +40,8 @@ def training_loop(imu, ann, hyperparams:dict):
         
     plt.figure()
     plt.title('MSE Loss')
-    plt.plot(range(hyperparams['epochs']), train_loss_history, label='train loss')
+    # plt.plot(range(hyperparams['epochs']), train_loss_history, label='train loss')
+    plt.plot(range(len(train_loss_history)), train_loss_history, label='train loss')
     plt.xlabel('Epochs')
     plt.ylabel('Loss')
     plt.legend()
@@ -51,52 +50,11 @@ def training_loop(imu, ann, hyperparams:dict):
 
 if __name__ == '__main__':
     data_dict = read_all_data()
-    
     imu = data_dict['imu'].to_numpy()
     ann = data_dict['ann'].to_numpy().flatten()
-    
     del data_dict # Remove to free memory
-    
-    # imu = pd.read_csv('processed_training_data/subject_001_01__x.csv')
-    # imu_t = pd.read_csv('processed_training_data/subject_001_01__x_time.csv')
-    # ann = pd.read_csv('processed_training_data/subject_001_01__y.csv')
-    # ann_t = pd.read_csv('processed_training_data/subject_001_01__y_time.csv')
-    
     
     with open('NN/nn_hyperparams.yaml', 'r') as f:
         hyperparams = yaml.safe_load(f)
-        
-    # # imu_new = imu
-    # # ann_new = ann
     
     training_loop(imu, ann, hyperparams)
-        
-    # training_loop(imu.to_numpy(), ann.to_numpy().reshape(len(ann),), hyperparams)
-    
-    
-    # imu = pd.read_csv('training_data/subject_001_01__x.csv',
-    #               names=['acc_x', 'acc_y', 'acc_z', 'gyro_x', 'gyro_y', 'gyro_z'])
-    # imu_t = pd.read_csv('training_data/subject_001_01__x_time.csv', names=['time'])
-    # ann = pd.read_csv('training_data/subject_001_01__y.csv', names=['labels'])
-    # ann_t = pd.read_csv('training_data/subject_001_01__y_time.csv', names=['time'])
-    
-    # expanded_ann = expand_ann(
-    #     imu_t['time'].tolist(),
-    #     ann['labels'].tolist(),
-    #     ann_t['time'].tolist()
-    # )
-    
-    # ann = expanded_ann['ann']
-    # ann_t = expanded_ann['ann_time']
-    
-    # with open('NN/nn_hyperparams.yaml', 'r') as f:
-    #     hyperparams = yaml.safe_load(f)
-        
-    # print(imu.to_numpy().shape)
-    # print(imu_new.to_numpy().shape)
-    
-    # print(ann_new.to_numpy().reshape(len(ann_new),).shape)
-    # print(np.array(ann).shape)
-        
-    # training_loop(imu.to_numpy(), np.array(ann), hyperparams)
-    
